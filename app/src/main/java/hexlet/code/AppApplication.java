cd9b4663fbd904c80c61168de158ded80b2efe99
Andrey Borisov <royalxxx@yandex.ru>
2024-03-15 14:28:30 +0400

    Fix java CI code & Makefile

--- app/src/main/java/hexlet/code/AppApplication.java
+++ app/src/main/java/hexlet/code/AppApplication.java
@@ -11,7 +11,6 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 @EnableJpaAuditing
 @SpringBootApplication
 public class AppApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(AppApplication.class, args);
 	}
